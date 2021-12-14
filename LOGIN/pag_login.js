f0a4c67b8cdc9de364111216ee34a050fa69ae7d
Lucca Sabino <94568969+LuccaSabino@users.noreply.github.com>
2021-12-14 16:42:13 -0300

    projeto-ficha (pag de login completa)

---
+++ LOGIN/pag_login.js
@@ -0,0 +1,13 @@
+function login() {
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+
+    if (email != "" && senha != "") {
+        if (email == "carecalegendario@basesocial.org" && senha == "1234") {
+            window.location.href = "/FICHA/ficha.html";
+        } else {
+            alert("Algo deu errado, verifique o email e a senha! :(")
+        }
+    } else { }
+}
+
